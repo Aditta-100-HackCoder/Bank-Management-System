f28c476a5892c70c077d2e34db2308ae9526edd8
Aditta baishnab <bijoybaishnab42@gmail.com>
2022-08-27 01:04:13 +0600

    I added another file

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php 
+ 
+   $con = mysqli_connect("localhost","root","","mybank"); 
+  /*  if($con){
+     echo"Successfully connected";
+ } 
+
+  else{
+      echo"didn't connect";
+  }
+
+?>
+*/
+?>
